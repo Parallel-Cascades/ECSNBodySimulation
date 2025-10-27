0d61709c1f40ff67972828aa2a11376f76fc4516
tirolinko <emk.tonev@gmail.com>
2025-10-27 21:04:55 +0200

    RotationOnAxisSystem.cs added

---
+++ Assets/ParallelCascades/ECSNBodySimulation/Runtime/ComponentData/RotationOnAxisData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace ParallelCascades.ECSNBodySimulation.Runtime.ComponentData
+{
+    public struct RotationOnAxisData : IComponentData
+    {
+        public float3 Value;
+    }
+}
